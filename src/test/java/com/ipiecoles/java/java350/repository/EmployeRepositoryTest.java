a8ae77d1bb071231b75e3d8c669fd0ae672805e2
Damien ARCO <damien.arco@cgi.com>
2020-03-09 16:55:58 +0100

    Finalisation test employeRepository

--- src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -3,6 +3,8 @@ package com.ipiecoles.java.java350.repository;
 
 import com.ipiecoles.java.java350.model.Employe;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,22 +16,42 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 public class EmployeRepositoryTest {
     @Autowired
     EmployeRepository employeRepository;
+
+    @BeforeEach
+    @AfterAll
+    public void setup() {
+        employeRepository.deleteAll();
+    }
+
     @Test
-    public void testFindLastMatricule() {
+    public void testFindLastMatricule0Employes() {
         //Given
-        Employe employe1, employe2;
+
+        //When
+        String res = employeRepository.findLastMatricule();
+        //Then
+        Assertions.assertThat(res).isNull();
+    }
+
+    @Test
+    public void testFindLastMatricule3Employes() {
+        //Given
+        Employe employe1, employe2, employe3;
         employe1 = new Employe();
         employe2 = new Employe();
-        employe1.setMatricule("M12345");
-        employe2.setMatricule("M23456");
+        employe3 = new Employe();
+        employe1.setMatricule("M00001");
+        employe2.setMatricule("M00003");
+        employe3.setMatricule("T00002");
 
         employeRepository.save(employe1);
         employeRepository.save(employe2);
+        employeRepository.save(employe3);
 
         //When
         String res = employeRepository.findLastMatricule();
 
         //Then
-        Assertions.assertThat(res).isEqualTo("23456");
+        Assertions.assertThat(res).isEqualTo("00003");
     }
 }
